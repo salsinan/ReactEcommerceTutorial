791525e933beec5418b1b02ae08c88c3d5148c2a
sarah alsinan <salsinan@protonmail.com>
2022-08-24 11:39:54 -0700

    render methods

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -8,31 +8,36 @@ export default class MainContent extends Component {
                     id: 1, 
                     name: "Leon", 
                     phone:"051-313", 
-                    address: {city: "Beaverton"}
+                    address: {city: "Beaverton"},
+                    photo: "https://picsum.photos/id/1010/60"
                 },
                 {
                     id: 2, 
                     name: "Leona", 
                     phone:"123-456", 
-                    address: {city: "Aloha"}
+                    address: {city: "Aloha"},
+                    photo: "https://picsum.photos/id/1011/60"
                 },
                 {
                     id: 3, 
                     name: "Loene", 
                     phone:null, 
-                    address: {city: "Clackamas"}
+                    address: {city: "Clackamas"},
+                    photo: "https://picsum.photos/id/1012/60"
                 },
                 {
                     id: 4, 
                     name: "Loena", 
                     phone:"051-320", 
-                    address: {city: "Milwaukie"}
+                    address: {city: "Milwaukie"},
+                    photo: "https://picsum.photos/id/1013/60"
                 },
                 {
                     id: 5, 
                     name: "Loops", 
                     phone:"051-332", 
-                    address: {city: "Tigard"}
+                    address: {city: "Tigard"},
+                    photo: "https://picsum.photos/id/1014/60"
                 }
               ]
     };
@@ -40,13 +45,38 @@ export default class MainContent extends Component {
     onRefresh=() => {
         this.setState({
             customerCount: 7
-        })
+        });
+    }
+
+    getPhoneToRender = (phone) => {
+        {
+            return !phone? (
+            <div className="bg-warning p-2">N/A</div>
+            ):(
+                phone
+            )}
+    }
+
+    getCustomerRow = () => {
+        {
+            return (this.state.customers.map((cust) => {
+                return (
+                    <tr key={cust.id}>
+                        <td>{cust.id}</td>
+                        <td><img src={cust.photo} alt="Customer"/></td>
+                        <td>{cust.name}</td>
+                        <td>{this.getPhoneToRender(cust.phone)}</td>
+                        <td>{cust.address.city}</td>
+                    </tr>
+                )
+            }))
+        }
     }
 
     render() {
         return (
         <div>
-            <h4 className="border-bottom m-1 p-1">{ this.state.pageTitle }    
+            <h4 className="m-1 p-1">{ this.state.pageTitle }    
 
                 <span className="badge bg-secondary m-2">{this.state.customerCount}
                 </span>
@@ -61,25 +91,7 @@ export default class MainContent extends Component {
                         <th>City</th>
                     </tr>
                 </thead>
-                <tbody>
-                {
-                    this.state.customers.map((cust) => {
-                        return (
-                            <tr key={cust.id}>
-                                <td>{cust.id}</td>
-                                <td>{cust.name}</td>
-                                <td>
-                                    {!cust.phone? (
-                                    <div className="bg-warning p-2 text-center">N/A</div>
-                                    ):(
-                                        cust.phone
-                                    )}</td>
-                                <td>{cust.address.city}</td>
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
+                <tbody>{this.getCustomerRow()}</tbody>
             </table>
         </div>
         );
